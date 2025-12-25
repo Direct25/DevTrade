c3e8d093c7ffb07114ef3c464242b3a705a72378
Direct25 <aryankushwaha2536@gmail.com>
2025-12-25 11:40:37 +0530

    feat(transactions): add transaction routes and setup controller

--- server/src/controllers/transaction.controller.js
+++ server/src/controllers/transaction.controller.js
@@ -2,3 +2,34 @@ import express from 'express';
 import Transaction from '../models/transaction.model.js';
 import User from '../models/user.model.js';
 
+export const createTransaction = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export const getTransactionById = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export const getUserTransactions = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export const updateTransactionStatus = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}   
